14cf45e5c57a4343e8fe48fc08a2969256c193f9
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-14 17:21:17 -0300

    Add: ForbiddenChars method
    This method adds a pattern with a list of forbidden characters, like negated character classes in Regex.

--- RegexIntegrated/RegIn.cs
+++ RegexIntegrated/RegIn.cs
@@ -7,11 +7,18 @@ public static class RegIn
     public static string AnyChar(this string source)
         => source + ".";
     
-
     public static string AllowedChars(this string source, params char[] chars)
     {
         string? result = "[" + string.Concat(chars) + "]";
  
         return source + result;
     }
+
+    public static string ForbiddenChars(this string source, params char[] chars)
+    {
+        string? result = "[^" + string.Concat(chars) + "]";
+        
+        return source + result;
+    }
+    
 }
