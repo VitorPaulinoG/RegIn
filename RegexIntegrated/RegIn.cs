90083574b49b14aab33b01335ab8fb753c08abbc
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-14 16:50:53 -0300

    Add: AllowedChars method
    This method adds a pattern with a list of allowed characters.

--- RegexIntegrated/RegIn.cs
+++ RegexIntegrated/RegIn.cs
@@ -7,5 +7,12 @@ public static class RegIn
     public static string AnyChar(this string source)
     {
         return source + ".";
-    } 
+    }
+
+    public static string AllowedChars(this string source, params char[] chars)
+    {
+        string? result = "[" + string.Concat(chars) + "]";
+ 
+        return source + result;
+    }
 }
