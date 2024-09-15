07b4e862ea6fea31ca0d009a9601f426bca8781a
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-14 22:02:23 -0300

    Change: all base methods to private
    Make private all methods that do not receive a pattern as a parameter, which just appends a metacharacter

--- RegexIntegrated/RegIn.cs
+++ RegexIntegrated/RegIn.cs
@@ -24,30 +24,30 @@ public static class RegIn
         return source + result;
     }
 
-    public static string Optional(this string source) => source + "?";
+    private static string Optional(this string source) => source + "?";
 
     public static string Optional(this string source, string optionalPattern)
         => Grouping(source, optionalPattern).Optional();
 
-    public static string Grouping(this string source, string pattern)
+    private static string Grouping(this string source, string pattern)
     {
         string result = "(" + pattern + ")";
         
         return source + result;
     }
 
-    public static string Star(this string source) => source + "*";
+    private static string Star(this string source) => source + "*";
 
     public static string Star(this string source, string pattern)
         => Grouping(source, pattern).Star();
 
-    public static string Many(this string source)
+    private static string Many(this string source)
         => source + "+";
 
     public static string Many(this string source, string pattern)
         => Grouping(source, pattern).Many();
 
-    public static string Range(this string source, uint min, uint? max = null)
+    private static string Range(this string source, uint min, uint? max = null)
     {
         if (max == null)
             return source + "{" + min + ",}";
@@ -57,13 +57,13 @@ public static class RegIn
     public static string Range(this string source, string pattern, uint min, uint? max = null)
         => Grouping(source, pattern).Range(min, max);
     
-    public static string Repeat(this string source, uint count)
+    private static string Repeat(this string source, uint count)
         => source + "{" + count + "}";
 
     public static string Repeat(this string source, string pattern, uint count)
         => Grouping(source, pattern).Repeat(count);
 
-    public static string StartWith(this string source)
+    private static string StartWith(this string source)
         => "^" + source;
 
     public static string StartWith(this string source, string pattern)
